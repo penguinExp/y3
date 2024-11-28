082efdca17057d190eefe6d4d88ac4ed3237fc56
adii <77727680+frozen-beak@users.noreply.github.com>
2024-11-28 23:20:23 +0530

    updated regex patterns

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use regex::Regex;
+
 ///
 /// Struct to represent the position of the [Token] in the input file
 ///
@@ -93,6 +100,62 @@ impl Token {
 }
 
 ///
+/// A structure holding [Regex] patterns to be used while parsing
+///
+#[derive(Debug)]
+struct Patterns {
+    ///
+    /// List of [Regex] patterns to be ignored while parsing.
+    ///
+    /// Fallowing types are ignored,
+    /// - Link's, URL's,
+    /// - File Paths
+    /// - Direct numbers like "1234"
+    /// - Email like patterns
+    /// - Regular Expressions
+    ///
+    ignore_patterns: Vec<Regex>,
+
+    ///
+    /// A [Regex] pattern to match against potential tokens
+    ///
+    word_pattern: Regex,
+
+    ///
+    /// A [Regex] pattern to split words to form tokens
+    ///
+    /// Useful while separating words like,
+    /// - `snake_case` to ["snake", "case"]
+    /// - `Get-Item` to ["Get", "Item"]
+    /// - `run—but` to ["run", "but"]
+    ///
+    split_pattern: Regex,
+}
+
+impl Patterns {
+    fn new() -> Self {
+        Self {
+            ignore_patterns: vec![
+                // URLs
+                Regex::new(r"https?://\S+").unwrap(),
+                // File paths
+                Regex::new(r"[\w\-\.]+(/[\w\-\.]+)+").unwrap(),
+                // Pure numbers
+                Regex::new(r"\b\d+\b").unwrap(),
+                // Regex patterns
+                Regex::new(r"\\[a-zA-Z]+[{[^()]+}]*").unwrap(),
+                // Email-like patterns
+                Regex::new(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b").unwrap(),
+            ],
+            // potential tokens
+            word_pattern: Regex::new(r"[a-zA-Z]+[0-9]*[a-zA-Z]*").unwrap(),
+            // split formats like -, _, etc.
+            split_pattern: Regex::new(r"[ _\-—]").unwrap(),
+        }
+    }
+}
+
+///
 /// A custom tokenizer which reads through the input file and parses words
 /// to be spell checked as [Token]'s
 ///
@@ -102,6 +165,11 @@ pub struct Tokenizer {
     /// List of parsed tokens from the input file
     ///
     tokens: Vec<Token>,
+
+    ///
+    /// Set of [Regex] patterns used for parsing tokens
+    ///
+    patterns: Patterns,
 }
 
 impl Tokenizer {
@@ -110,5 +178,51 @@ impl Tokenizer {
     ///
     pub fn tokens(&self) -> &[Token] {
         &self.tokens
+    }
+
+    ///
+    /// Create an instance of [Tokenizer]
+    ///
+    /// # Example
+    ///
+    /// ```rs
+    ///  let mut tokenizer = Tokenizer::new();
+    ///  assert_eq!(tokenizer.tokens().len(), 0 as usize);
+    /// ```
+    ///
+    pub fn new() -> Self {
+        Self {
+            tokens: Vec::new(),
+            patterns: Patterns::new(),
+        }
+    }
+
+    ///
+    /// Clear the list of parsed [Token]'s
+    ///
+    /// ## NOTE
+    ///
+    /// It has no effect on the allocated memory for the [Vec].
+    ///
+    /// This saves the overhead of reallocating the memory again, it
+    /// simply uses pre-allocated memory for upcoming tokens.
+    ///
+    pub fn clear_tokens(&mut self) {
+        self.tokens.clear();
+    }
+
+    pub fn tokenize(&mut self, file_path: &str) -> io::Result<()> {
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+
+        for (line_no, line) in reader.lines().enumerate() {
+            let line = line?;
+            let mut pos: usize = 0;
+
+            // split lines
+            for chunk in self.patterns.split_pattern.split(&line) {}
+        }
+
+        Ok(())
     }
 }
